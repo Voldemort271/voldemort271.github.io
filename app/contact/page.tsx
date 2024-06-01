bd5ebb5c26cb4a8bef93465c2080f276444381db
Shubham <shubhampadhi271205@gmail.com>
2024-06-01 10:01:00 +0530

    Contacts page done

---
+++ app/contact/page.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Gamepad, Instagram, Mail, Phone } from "lucide-react";
+
+const Contacts = () => {
+  return (
+    <div className={"layout-animate"}>
+      <div className={"w-full md:max-w-screen-sm m-10 ml-0"}>
+        <div
+          className={
+            "font-bold text-5xl text-slate-950 bg-gradient-to-tl from-pink-500" +
+            " to-violet-500" +
+            " inline-block text-transparent bg-clip-text"
+          }
+        >
+          Contact me
+        </div>
+        <div
+          className={
+            "my-2.5 hidden sm:block font-semibold text-base leading-5 text-slate-600"
+          }
+        >
+          I&apos;m a relatively busy person but you can still contact me via the
+          following:
+        </div>
+      </div>
+      <div
+        className={
+          "mb-5 w-full flex flex-col items-center gap-5 lg:max-w-screen-sm"
+        }
+      >
+        <Alert className={"bg-slate-100"}>
+          <Phone className="h-4 w-4" />
+          <AlertTitle>Phone</AlertTitle>
+          <AlertDescription>0987654321</AlertDescription>
+        </Alert>
+        <Alert className={"bg-slate-100"}>
+          <Mail className="h-4 w-4" />
+          <AlertTitle>Email</AlertTitle>
+          <AlertDescription>a@b.c</AlertDescription>
+        </Alert>
+        <Alert className={"bg-slate-100"}>
+          <Gamepad className="h-4 w-4" />
+          <AlertTitle>Discord</AlertTitle>
+          <AlertDescription>@ur_mom</AlertDescription>
+        </Alert>
+        <Alert className={"bg-slate-100"}>
+          <Instagram className="h-4 w-4" />
+          <AlertTitle>Instagram</AlertTitle>
+          <AlertDescription>@bla</AlertDescription>
+        </Alert>
+      </div>
+    </div>
+  );
+};
+
+export default Contacts;
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const NotFound = () => {
-  return <div>Page not found.</div>;
+  return (
+    <div className={"layout-animate"}>
+      <div className={"w-full md:max-w-screen-sm m-10 ml-0"}>
+        <div
+          className={
+            "text-6xl text-slate-950 bg-gradient-to-tl font-extrabold from-pink-700 to-violet-700 inline-block" +
+            " text-transparent bg-clip-text"
+          }
+        >
+          404
+        </div>
+        <div className={"text-slate-500"}>
+          The page you requested was not found on our server.
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
